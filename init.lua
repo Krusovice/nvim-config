bf987313cf90e723e68a7b5e81341f76de81c4b0
Krusovice <jmk1589@gmail.com>
2026-03-04 22:12:05 +0100

    tree-sitter correctly setup

--- init.lua
+++ init.lua
@@ -1,47 +1,44 @@
+-- init.lua
+
+vim.cmd("filetype plugin indent on")
 vim.g.mapleader = " "
--- Show line numbers
+
+-- Line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
--- global clipboard, to yank paste outside nvim
 vim.opt.clipboard = "unnamedplus"
--- Disable swap files
 vim.opt.swapfile = false
--- Better search
-vim.opt.ignorecase = true  -- Case insensitive search
-vim.opt.smartcase = true   -- Unless you use capitals
--- Indentation for /JS/TS
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true   -- Use spaces instead of tabs
+vim.opt.expandtab = true
 vim.opt.smartindent = true
--- Better visual feedback
-vim.opt.signcolumn = "auto" -- Always show sign column (for LSP diagnostics)
+vim.opt.signcolumn = "auto"
 
 -- Terminal keybind
-vim.keymap.set('n', '<leader>t', ':vsplit | term<CR>:vertical resize 80<CR>i', { desc = 'Open terminal split' })
-vim.keymap.set('t', '<C-w>', '<C-\\><C-n><C-w>', { desc = 'Window switch from terminal' })
+vim.keymap.set('n', '<leader>t', ':vsplit | term<CR>:vertical resize 80<CR>i')
+vim.keymap.set('t', '<C-w>', '<C-\\><C-n><C-w>')
 
--- Move lines up/down
+-- Move lines
 vim.keymap.set("n", "<A-j>", ":m .+1<CR>==")
 vim.keymap.set("n", "<A-k>", ":m .-2<CR>==")
-
--- improved indentation in visual mode
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
 
--- switching to terminal view, starts in i-mode
+-- Terminal starts in insert
 vim.api.nvim_create_autocmd("BufEnter", {
   pattern = "term://*",
   command = "startinsert"
 })
 
--- filetype detection for buffere åbnet via netrw
-vim.api.nvim_create_autocmd({"BufRead", "BufEnter"}, {
+-- Web files 2 spaces
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "astro", "html", "css", "javascript", "typescript", "typescriptreact" },
   callback = function()
-    if vim.bo.filetype == "" then
-      vim.cmd("filetype detect")
-    end
-  end,
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+  end
 })
 
 -- Bootstrap lazy.nvim
@@ -55,93 +52,78 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- 2 spaces for web files
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "astro", "html", "css", "javascript", "typescript", "typescriptreact" },
-  callback = function()
-    vim.opt_local.tabstop = 2
-    vim.opt_local.shiftwidth = 2
-  end
-})
-
--- Load plugins
+-- Plugins
 require("lazy").setup({
-  -- Syntax highlighting
+
+  -- Treesitter
 {
   "nvim-treesitter/nvim-treesitter",
+  lazy = false,
   build = ":TSUpdate",
-  opts = {
-    ensure_installed = { "rust", "javascript", "typescript", "html", "css", "python", "astro" },
-    highlight = {
-      enable = true,
-    },
-  },
+  config = function()
+    require("nvim-treesitter").setup({
+      install_dir = vim.fn.stdpath("data") .. "/site",
+    })
+
+    require("nvim-treesitter").install({
+      "rust",
+      "javascript",
+      "typescript",
+      "html",
+      "css",
+      "python",
+      "astro",
+    })
+  end,
 },
-  -- LSP config
+
+  -- Autotag (afhængig af Treesitter)
+  {
+    "windwp/nvim-ts-autotag",
+    event = "BufReadPost",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+
+  -- Mason + LSP
   {
     "williamboman/mason-lspconfig.nvim",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "neovim/nvim-lspconfig",
-    },
+    dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
     config = function()
       require("mason").setup()
       require("mason-lspconfig").setup({
-        ensure_installed = {
-          "html",
-          "cssls",
-          "ts_ls",
-          "astro",
-          "rust_analyzer",
-          "pyright",
-        },
+        ensure_installed = { "html", "cssls", "ts_ls", "astro", "rust_analyzer", "pyright" },
         automatic_installation = true,
       })
     end,
   },
-  -- Enable language servers
+
+  -- Enable LSP
   {
     "neovim/nvim-lspconfig",
     config = function()
-      vim.lsp.enable({'html', 'cssls', 'ts_ls', 'astro', 'rust_analyzer', 'pyright'})
+      vim.lsp.enable({ "html", "cssls", "ts_ls", "astro", "rust_analyzer", "pyright" })
     end,
   },
+
   -- Autocompletion
   {
     "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-    },
+    dependencies = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip" },
     config = function()
       local cmp = require("cmp")
       local luasnip = require("luasnip")
       cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
+        snippet = { expand = function(args) luasnip.lsp_expand(args.body) end },
         mapping = cmp.mapping.preset.insert({
           ["<Tab>"] = cmp.mapping.select_next_item(),
           ["<S-Tab>"] = cmp.mapping.select_prev_item(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "buffer" },
-        },
+        sources = { { name = "nvim_lsp" }, { name = "luasnip" }, { name = "buffer" } },
       })
     end,
   },
--- Auto close HTML tags
-  {
-    "windwp/nvim-ts-autotag",
-    config = function()
-      require("nvim-ts-autotag").setup()
-    end,
-  },
+
 })
